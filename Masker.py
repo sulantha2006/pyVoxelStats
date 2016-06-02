b69cffb325be6a1e1f567016b3b1b91d6858c083
Sulantha <sulantha.s@gmail.com>
2016-06-02 16:44:41 -0400

    Added Data Matrix Class

--- Masker.py
+++ Masker.py
@@ -1,15 +1,15 @@
-import nibabel
+import nibabel, numpy
 
 class Masker:
     def __init__(self, file_type, mask_file):
         self.file_type = file_type
         self.mask_file = mask_file
-        self.mask = self.read_mask()
+        self.mask, self.mask_array = self.read_mask()
 
     def read_mask(self):
         img = nibabel.load(self.mask_file)
         mask_data = img.get_data()
-        return mask_data > 0.9
+        return img, mask_data > 0.9
 
     def mask_image(self, image_file):
         img = nibabel.load(image_file)
@@ -17,9 +17,15 @@ class Masker:
         return self.mask_data(image_data)
 
     def mask_data(self, data):
-        return data[self.mask]
+        return data[self.mask_array]
 
     def rebuild_image(self, data):
-        new_image = data.reshape(self.mask.shape)
+        new_image_data = numpy.empty(self.mask_array.shape, dtype=data.dtype)
+        new_image_data[self.mask_array] = data
+        affine = self.mask.affine
+        if self.file_type == 'minc':
+            new_image = nibabel.Minc2Image(new_image_data, affine)
+        elif self.file_type == 'nifti':
+            new_image = nibabel.Nifti1Image(new_image_data, affine)
         return new_image
 
--- Test/TestMasker.py
+++ Test/TestMasker.py
@@ -4,4 +4,4 @@ import nibabel
 masker = Masker('minc', 'VoxelStatsTestData/Masks/FullBrain.mnc')
 image_masked = masker.mask_image('VoxelStatsTestData/MINC/I300779.mnc')
 rebuilt = masker.rebuild_image(image_masked)
-nibabel.save(rebuilt, 'Test/MaskerRebuilt.mnc')
+rebuilt.to_filename('Test/MaskerRebuilt.mnc')
