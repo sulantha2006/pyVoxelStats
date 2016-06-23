d456c36c6624a88bcf9aef586d29d5262e815391
Sulantha <sulantha.s@gmail.com>
2016-06-23 14:16:42 -0400

    Adding mods on Data csv

--- pyVoxelStatsLM.py
+++ pyVoxelStatsLM.py
@@ -3,7 +3,7 @@ from Util.Masker import Masker
 from Util.VoxelOperation import VoxelOperation
 
 
-string_model = 'A ~ e + C + C(F)'
+string_model = 'A ~ e + C + C(f)'
 data_file = 'VoxelStatsTestData/CSV/Data.csv'
 mask_file = 'VoxelStatsTestData/Masks/FullBrain.mnc'
 voxel_vars = ['A', 'C']
