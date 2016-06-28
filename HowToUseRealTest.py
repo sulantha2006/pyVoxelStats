5a9c304625a31812aa67fed4e8e1d2b1c91d71b7
sulantha <sulantha.s@gmail.com>
2016-06-28 11:17:34 -0400

    Adding embarasingly parallel method. Testing.

--- HowToUseRealTest.py
+++ HowToUseRealTest.py
@@ -8,7 +8,7 @@ subset_string = 'diag_3bl == 2 & Baseline == 1'
 file_type = 'minc'
 
 lm = pyVoxelStatsLM(file_type, model_string, csv_file, mask_file, voxel_variables, subset_string)
-lm.set_up_cluster(profile_name='sgeov')
+lm.set_up_cluster(profile_name='sgeov', workers=215)
 results = lm.evaluate()
 
 lm.save('/home/sulantha/Desktop/Av45_bathbl_fromThraick_pyvs_speedtest_2.mnc', 'tvalues', 'AV45_pathbl')
